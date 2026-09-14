fddcc2111191995dea27e1a5ce99a627dc11ba43
Mohamed <elhello504714@gmail.com>
2026-09-14 03:24:18 +0300

    Fix hero slider image paths using imports

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -3,7 +3,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Link } from "react-router-dom";
-import { Autoplay , Pagination } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules";
+
+// 1. استيراد الصور هنا (تأكد أن المسار النسبي نسبي لمكان هذا الملف)
+import banner1 from "../img/banner_Hero1.jpg";
+import banner2 from "../img/banner_Hero2.jpg";
+import banner3 from "../img/banner_Hero3.jpg";
 
 function HeroSlider() {
   return (
@@ -11,13 +16,13 @@ function HeroSlider() {
       <div className="hero">
         <div className="container">
           <Swiper
-          loop={true}
+            loop={true}
             autoplay={{
               delay: 2500,
               disableOnInteraction: false,
             }}
-            pagination={true}
-            modules={[Pagination ,Autoplay ]}
+            pagination={{ clickable: true }}
+            modules={[Pagination, Autoplay]}
             className="mySwiper"
           >
             <SwiperSlide>
@@ -32,8 +37,10 @@ function HeroSlider() {
                 </Link>
               </div>
 
-              <img src="/src/img/banner_Hero1.jpg" alt="slider hero 1" />
+              {/* 2. استبدال المسار النصي بالمتغير */}
+              <img src={banner1} alt="slider hero 1" />
             </SwiperSlide>
+
             <SwiperSlide>
               <div className="content">
                 <h4>Introducing the new</h4>
@@ -46,8 +53,9 @@ function HeroSlider() {
                 </Link>
               </div>
 
-              <img src="/src/img/banner_Hero2.jpg" alt="slider hero 1" />
+              <img src={banner2} alt="slider hero 2" />
             </SwiperSlide>
+
             <SwiperSlide>
               <div className="content">
                 <h4>Introducing the new</h4>
@@ -60,7 +68,7 @@ function HeroSlider() {
                 </Link>
               </div>
 
-              <img src="/src/img/banner_Hero3.jpg" alt="slider hero 3" />
+              <img src={banner3} alt="slider hero 3" />
             </SwiperSlide>
           </Swiper>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
 import CartProvider from './components/context/CartContext.jsx'
+import './index.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
-    <CartProvider><App /></CartProvider>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
-    
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
